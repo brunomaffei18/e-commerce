1ec4fc8a1c442ad0ad6005ba46901fb6e16d2835
Buruno18 <maffeib18@gmail.com>
2023-09-02 16:44:15 -0300

    Entrega 2

--- js/login.js
+++ js/login.js
@@ -1,7 +1,8 @@
 const form = document.getElementById("formulario");
 
-var ultimoSegmento = window.location.pathname.split('/').pop();//separo el final de la ruta en la que se esta parado
+var ultimoSegmento = window.location.pathname.split('/').pop(); //separo el final de la ruta en la que se esta parado
 
+// Validar inicio de sesión
 if (ultimoSegmento == 'login.html') {
     form.addEventListener('submit', function (event) {
         const datos = document.getElementsByClassName("datos");
@@ -10,16 +11,31 @@ if (ultimoSegmento == 'login.html') {
             event.preventDefault();
             window.location.href = "index.html";
             sessionStorage.setItem("logueo", "true");
+            localStorage.setItem("usuario", datos[0].value);
         }
     });
 }
 
+// Verificar logueo
 function verificarLogueo() {
 
     if (ultimoSegmento !== 'login.html' && sessionStorage.getItem("logueo") !== "true") {
             window.location.href = "login.html";
-        }
+    } else {
+        const display = document.getElementById("displayusuario");
+        const datos = localStorage.getItem("usuario");
+        
+        display.innerHTML = `<i class="bi bi-person-fill me-2" style="font-size: 20px"></i>` + datos;
+        
+        display.setAttribute("href", "my-profile.html");
+        const removeSesion = document.getElementById("removeSesion");
+        
+        removeSesion.addEventListener("click", ()=>{
+            sessionStorage.removeItem("logueo");
+        });
+    }
     
 }
-window.onload = verificarLogueo;
+
+window.onload = verificarLogueo; // Al cargar la página la primera función que carga es verificarLogueo
 
--- js/products.js
+++ js/products.js
@@ -1,55 +1,152 @@
 const container = document.querySelector("#container-content"); // Constante que toma el contenedor div del HTML.
+const asc = document.getElementById("ascendente"); // Boton filtro precio ascendente
+const desc = document.getElementById("descendente"); // Boton filtro precio descendente
+const rel = document.getElementById("relevancia"); // Boton filtro relevancia descendente
+const rangoPrecio = document.getElementById("rangoPrecio"); // Boton filtro rango de precio
+const limpiarFiltros = document.getElementById("limpiarFiltros"); // Boton limpiar filtros
 
-const url = "https://japceibal.github.io/emercado-api/cats_products/101.json"; // URL de JSON con información de los autos.
+const id = localStorage.getItem("catID");
+const url = ("https://japceibal.github.io/emercado-api/cats_products/"+ id +".json"); // URL con los JSON de todas las categorías
 
-    fetch(url)
+
+        // FETCH
+
+        fetch(url)
         .then(response => response.json())
         .then(data => {
-            const products = data.products; // Constante para trabajar sobre la info de cada producto.
-            products.forEach(product => {
-                const containerCard = document.createElement("div");
-                containerCard.classList.add("col-xl-4", "col-12", "col-md-6", "col-lg-3", "container-products");
+            let products = data.products; // Constante para trabajar sobre la info de cada producto.
+            
+            // Función que muestra los productos
+            function showProducts(array){
+                let content = "";
+            
+            if(array.length > 0){
+                array.forEach(product => {
+                    content += 
+                    `
+                    <div class="col-xl-4 col-12 col-md-6 col-lg-3 container-products">
+                        <div class="card col-12 div-products">
+                            <img class="card-image image-products" src="${product.image}">
+                            <h2 class="card-title title-products">${product.name}</h2>
+                            <p class="card-description description-products">${product.description}</p>
+                            <p class="card-cost cost-products">Precio: ${product.currency} ${product.cost}</p>
+                            <p class="card-soldcount soldCount-products">Cantidad vendida: ${product.soldCount}</p>
+                        </div>
+                    </div>
+                    `;
+                container.innerHTML = content;
+                });
+                
+            } else {
+                // Alerta para cuando no se encuentran productos
+                container.innerHTML = 
+                    `<div class="alert-danger bg-danger alert-error-filter">No se encontraron productos</div>`;
+            }
+            }
 
-                // CREAR ELEMENTO DIV (CONTENEDOR PRODUCTO)
-                const productDiv = document.createElement("div");
-                productDiv.classList.add("card", "col-12", "div-products");
+            showProducts(products);
 
-                // CREAR ELEMENTO IMAGEN
-                const image = document.createElement("img");
-                image.src = product.image;
-                image.classList.add("card-image", "image-products");
-                productDiv.appendChild(image);
 
-                // CREAR ELEMENTO H2 (TITULO)
-                const name = document.createElement("h2");
-                name.textContent = product.name;
-                name.classList.add("card-title", "title-products");
-                productDiv.appendChild(name);
+            // FILTROS
 
-                // CREAR ELEMENTO P (DESCRIPCIÓN)
-                const description = document.createElement("p");
-                description.textContent = product.description;
-                description.classList.add("card-description", "description-products");
-                productDiv.appendChild(description);
+            // Filtrar precio ascendente
+            asc.addEventListener("click", ()=>{
+                
+                let ascendente =   
+                products.sort((a, b) => {
+                    if (a.cost < b.cost) {
+                      return -1;
+                    }
+                });
 
-                // CREAR ELEMENTO P (COSTO PRODUCTO)
-                const cost = document.createElement("p");
-                cost.textContent = "Precio: " + product.currency + "" + product.cost;
-                cost.classList.add("card-cost", "cost-products");
-                productDiv.appendChild(cost);
+                showProducts(ascendente);
 
-                // CREAR ELEMENTO P (CANTIDAD VENDIDOS)
-                const soldCount = document.createElement("p");
-                soldCount.textContent = "Cantidad vendida: " + product.soldCount;
-                soldCount.classList.add("card-soldCount", "soldCount-products");
-                productDiv.appendChild(soldCount);
-
-                // AGREGAR ELEMENTOS AL HTML PARA PODER VISUALIZARLOS
-                containerCard.appendChild(productDiv);
-                container.appendChild(containerCard);
             });
+
+
+            // Filtrar precio descendente
+            desc.addEventListener("click", ()=>{
+
+                let descendente =   
+                products.sort((a, b) => {
+                    if (a.cost > b.cost) {
+                      return -1;
+                    }
+                });
+
+                showProducts(descendente);
+
+            });    
+
+
+            // Filtrar por relevancia descendente
+            rel.addEventListener("click", ()=>{
+
+                let relevancia =   
+                products.sort((a, b) => {
+                    if (a.soldCount > b.soldCount) {
+                      return -1;
+                    }
+                });
+
+                showProducts(relevancia);
+
+            });    
+
+
+            // Filtrar por rango de precio
+            rangoPrecio.addEventListener("click", ()=>{
+
+                let precioMin = document.getElementById("precioMinimo").value;
+                let precioMax = document.getElementById("precioMaximo").value;
+
+                let productosRangoPrecio =      
+                products.filter((product) => product.cost >= precioMin && product.cost <= precioMax);
+
+                showProducts(productosRangoPrecio);
+
+            });
+
+
+            // Limpiar filtros
+            limpiarFiltros.addEventListener("click", ()=>{
+
+                showProducts(products);
+
+            }); 
+            
+            // FIN FILTROS
+            
         })
         // Mensaje de error por si ocurre un error al cargar el fetch.
         .catch(error => {
             console.error("Error al cargar los productos:", error);
         });
+
+        //FIN FETCH
+
+
+        // BUSCADOR DE PRODUCTOS
+
+        function buscarProductos() {
+            const input = document.getElementById("buscar"); // Obtener input search
+            let card = document.getElementsByClassName("col-xl-4 col-12 col-md-6 col-lg-3 container-products");
+            let nombres = document.getElementsByClassName("card-title title-products"); // Obtener el texto del elemento en mayusculas
+            let descripciones = document.getElementsByClassName("card-description description-products");
+            
+            // Mostrar u ocultar el elemento según si coincide con el texto ingresado
+            for(let i=0; i<nombres.length; i++){
+                let descripcion = descripciones[i].textContent.toUpperCase();
+                let nombre = nombres[i].textContent.toUpperCase();
+        
+                if (descripcion.includes(input.value.toUpperCase()) || 
+                    nombre.includes( input.value.toUpperCase() )) 
+                {
+                    card[i].style.display = 'block'; // Mostrar el elemento
+                } else {
+                    card[i].style.display = 'none'; // Ocultar el elemento
+                }
+            }
+        }
+        
+        // FIN BUSCADOR DE PRODUCTOS
